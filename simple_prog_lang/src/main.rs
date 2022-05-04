5f5f6d7f3d8b132cdb17c300378079954a04141f
Takeo Kondo <takeoverjp@gmail.com>
2022-05-04 14:29:03 +0900

    feat: support function

--- simple_prog_lang/src/main.rs
+++ simple_prog_lang/src/main.rs
@@ -7,66 +7,110 @@ enum EvalError {
     InvalidProgram,
 }
 
-fn eval(program: &str, index: &mut usize) -> Result<i32, EvalError> {
-    let mut chrs = program.chars().skip(*index);
-    if let Some(c) = chrs.next() {
-        *index += 1;
-        match c {
-            // number
-            '0'..='9' => {
-                let mut val = c as i32 - '0' as i32;
-                while let Some(c) = chrs.next() {
-                    if c.is_digit(10) {
-                        *index += 1;
-                        val = val * 10 + (c as i32 - '0' as i32);
+struct Parser {
+    map: std::collections::HashMap<char, String>,
+}
+impl Parser {
+    pub fn new() -> Parser {
+        Parser {
+            map: std::collections::HashMap::new(),
+        }
+    }
+    pub fn eval(
+        &mut self,
+        program: &str,
+        index: &mut usize,
+        arg: Option<i32>,
+    ) -> Result<i32, EvalError> {
+        let mut chrs = program.chars().skip(*index);
+        if let Some(c) = chrs.next() {
+            *index += 1;
+            match c {
+                // function
+                'A'..='Z' => {
+                    if let Some(next) = chrs.next() {
+                        match next {
+                            // function definition
+                            '[' => {
+                                *index += 1;
+                                let last = program.find(']').unwrap();
+                                self.map.insert(c, program[*index..last].to_string());
+                                self.eval(program, &mut (last + 1), arg)
+                            }
+                            // function call
+                            '(' => {
+                                *index += 1;
+                                let p = self.map.get(&c).unwrap().clone();
+                                let arg = Some(self.eval(program, index, arg)?);
+                                self.eval(p.as_str(), &mut 0, arg)
+                            }
+                            _ => Err(EvalError::InvalidProgram),
+                        }
                     } else {
-                        break;
+                        Err(EvalError::InvalidProgram)
                     }
                 }
-                Ok(val)
-            }
 
-            // skip spaces
-            ' ' => {
-                while let Some(c) = chrs.next() {
-                    if c.is_whitespace() {
-                        *index += 1;
-                    } else {
-                        break;
-                    }
-                }
-                eval(&program, index)
-            }
+                // function arg
+                '.' => Ok(arg.unwrap()),
 
-            // operators
-            '+' | '-' | '*' | '/' => {
-                let lhs = eval(&program, index)?;
-                let rhs = eval(&program, index)?;
-                match c {
-                    '+' => Ok(lhs + rhs),
-                    '-' => Ok(lhs - rhs),
-                    '*' => Ok(lhs * rhs),
-                    '/' => {
-                        if rhs == 0 {
-                            Err(EvalError::ZeroDivision)
+                // number
+                '0'..='9' => {
+                    let mut val = c as i32 - '0' as i32;
+                    while let Some(c) = chrs.next() {
+                        if c.is_digit(10) {
+                            *index += 1;
+                            val = val * 10 + (c as i32 - '0' as i32);
                         } else {
-                            Ok(lhs / rhs)
+                            break;
                         }
                     }
-                    _ => Err(EvalError::UnknownCharacter),
+                    Ok(val)
                 }
+                // skip spaces
+                ' ' => {
+                    while let Some(c) = chrs.next() {
+                        if c.is_whitespace() {
+                            *index += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    self.eval(program, index, arg)
+                }
+                // operators
+                '+' | '-' | '*' | '/' => {
+                    let lhs = self.eval(program, index, arg)?;
+                    let rhs = self.eval(program, index, arg)?;
+                    match c {
+                        '+' => Ok(lhs + rhs),
+                        '-' => Ok(lhs - rhs),
+                        '*' => Ok(lhs * rhs),
+                        '/' => {
+                            if rhs == 0 {
+                                Err(EvalError::ZeroDivision)
+                            } else {
+                                Ok(lhs / rhs)
+                            }
+                        }
+                        _ => Err(EvalError::UnknownCharacter),
+                    }
+                }
+                _ => Err(EvalError::UnknownCharacter),
             }
-            _ => Err(EvalError::UnknownCharacter),
+        } else {
+            Err(EvalError::InvalidProgram)
         }
-    } else {
-        Err(EvalError::InvalidProgram)
     }
 }
-
 fn main() {
     match env::args().nth(1) {
         Some(program) => {
-            println!("{} = {}", program, eval(&program, &mut 0).ok().unwrap());
+            println!(
+                "{} = {}",
+                program,
+                Parser::new().eval(&program, &mut 0, None).ok().unwrap()
+            );
         }
         _ => {
             eprintln!("[USAGE] {} PROGRAM", env::args().nth(0).unwrap());
@@ -76,66 +120,78 @@ fn main() {
 
 #[test]
 fn test_numbers() {
-    assert_eq!(Ok(0), eval("0", &mut 0));
-    assert_eq!(Ok(1), eval("1", &mut 0));
-    assert_eq!(Ok(123), eval("123", &mut 0));
+    assert_eq!(Ok(0), Parser::new().eval("0", &mut 0, None));
+    assert_eq!(Ok(1), Parser::new().eval("1", &mut 0, None));
+    assert_eq!(Ok(123), Parser::new().eval("123", &mut 0, None));
 }
 
 #[test]
 fn test_add() {
-    assert_eq!(Ok(2), eval("+ 1 1", &mut 0));
-    assert_eq!(Ok(2), eval("+ 1   1", &mut 0));
-    assert_eq!(Ok(3), eval("+ 1 2", &mut 0));
-    assert_eq!(Ok(579), eval("+ 123 456", &mut 0));
-    assert_eq!(Ok(1000), eval("+ 999 1", &mut 0));
+    assert_eq!(Ok(2), Parser::new().eval("+ 1 1", &mut 0, None));
+    assert_eq!(Ok(2), Parser::new().eval("+ 1   1", &mut 0, None));
+    assert_eq!(Ok(3), Parser::new().eval("+ 1 2", &mut 0, None));
+    assert_eq!(Ok(579), Parser::new().eval("+ 123 456", &mut 0, None));
+    assert_eq!(Ok(1000), Parser::new().eval("+ 999 1", &mut 0, None));
 }
 
 #[test]
 fn test_sub() {
-    assert_eq!(Ok(0), eval("- 1 1", &mut 0));
-    assert_eq!(Ok(-1), eval("- 1 2", &mut 0));
-    assert_eq!(Ok(-333), eval("- 123 456", &mut 0));
-    assert_eq!(Ok(998), eval("- 999 1", &mut 0));
+    assert_eq!(Ok(0), Parser::new().eval("- 1 1", &mut 0, None));
+    assert_eq!(Ok(-1), Parser::new().eval("- 1 2", &mut 0, None));
+    assert_eq!(Ok(-333), Parser::new().eval("- 123 456", &mut 0, None));
+    assert_eq!(Ok(998), Parser::new().eval("- 999 1", &mut 0, None));
 }
 
 #[test]
 fn test_mul() {
-    assert_eq!(Ok(1), eval("* 1 1", &mut 0));
-    assert_eq!(Ok(2), eval("* 1 2", &mut 0));
-    assert_eq!(Ok(492), eval("* 123 4", &mut 0));
-    assert_eq!(Ok(999), eval("* 999 1", &mut 0));
+    assert_eq!(Ok(1), Parser::new().eval("* 1 1", &mut 0, None));
+    assert_eq!(Ok(2), Parser::new().eval("* 1 2", &mut 0, None));
+    assert_eq!(Ok(492), Parser::new().eval("* 123 4", &mut 0, None));
+    assert_eq!(Ok(999), Parser::new().eval("* 999 1", &mut 0, None));
 }
 
 #[test]
 fn test_div() {
-    assert_eq!(Ok(1), eval("/ 1 1", &mut 0));
-    assert_eq!(Ok(0), eval("/ 1 2", &mut 0));
-    assert_eq!(Ok(2), eval("/ 246 123", &mut 0));
-    assert_eq!(Err(EvalError::ZeroDivision), eval("/ 999 0", &mut 0));
+    assert_eq!(Ok(1), Parser::new().eval("/ 1 1", &mut 0, None));
+    assert_eq!(Ok(0), Parser::new().eval("/ 1 2", &mut 0, None));
+    assert_eq!(Ok(2), Parser::new().eval("/ 246 123", &mut 0, None));
+    assert_eq!(
+        Err(EvalError::ZeroDivision),
+        Parser::new().eval("/ 999 0", &mut 0, None)
+    );
 }
 
 #[test]
 fn test_multi_operator() {
-    assert_eq!(Ok(10), eval("+ + + 1 2 3 4", &mut 0));
-    assert_eq!(Ok(14), eval("+ 2 *4 3", &mut 0));
-    assert_eq!(Ok(33), eval("+ 21* 4 3", &mut 0));
+    assert_eq!(Ok(10), Parser::new().eval("+ + + 1 2 3 4", &mut 0, None));
+    assert_eq!(Ok(14), Parser::new().eval("+ 2 *4 3", &mut 0, None));
+    assert_eq!(Ok(33), Parser::new().eval("+ 21* 4 3", &mut 0, None));
 }
 
 #[test]
-#[ignore]
 fn test_function() {
-    assert_eq!(Ok(2), eval("F[+ . .] F(1)", &mut 0));
-    assert_eq!(Ok(10), eval("F[* . 2] F(5)", &mut 0));
-    assert_eq!(Ok(16), eval("F[* . .] F(F(2))", &mut 0));
-    assert_eq!(Ok(256), eval("F[* . .] F(F(F(2)))", &mut 0));
+    assert_eq!(Ok(2), Parser::new().eval("+ . .", &mut 0, Some(1)));
+    assert_eq!(Ok(2), Parser::new().eval("F[+ . .] F(1)", &mut 0, None));
+    assert_eq!(Ok(10), Parser::new().eval("F[* . 2] F(5)", &mut 0, None));
+    assert_eq!(Ok(16), Parser::new().eval("F[* . .] F(F(2))", &mut 0, None));
+    assert_eq!(
+        Ok(256),
+        Parser::new().eval("F[* . .] F(F(F(2)))", &mut 0, None)
+    );
 }
 
 #[test]
 fn test_unknown_character() {
-    assert_eq!(Err(EvalError::UnknownCharacter), eval("?", &mut 0));
+    assert_eq!(
+        Err(EvalError::UnknownCharacter),
+        Parser::new().eval("?", &mut 0, None)
+    );
 }
 
 #[test]
 fn test_invalid_program() {
-    assert_eq!(Err(EvalError::InvalidProgram), eval("", &mut 0));
+    assert_eq!(
+        Err(EvalError::InvalidProgram),
+        Parser::new().eval("", &mut 0, None)
+    );
 }
