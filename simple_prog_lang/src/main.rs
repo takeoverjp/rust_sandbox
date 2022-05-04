ef99d61d50cb7d2748565a13142707a3e0061cde
Takeo Kondo <takeoverjp@gmail.com>
2022-05-04 14:56:30 +0900

    feat: support multi arg function

--- simple_prog_lang/src/main.rs
+++ simple_prog_lang/src/main.rs
@@ -20,7 +20,7 @@ impl Parser {
         &mut self,
         program: &str,
         index: &mut usize,
-        arg: Option<i32>,
+        args: Vec<i32>,
     ) -> Result<i32, EvalError> {
         let mut chrs = program.chars().skip(*index);
         if let Some(c) = chrs.next() {
@@ -35,14 +35,18 @@ impl Parser {
                                 *index += 1;
                                 let last = *index + program[*index..].find(']').unwrap();
                                 self.map.insert(c, program[*index..last].to_string());
-                                self.eval(program, &mut (last + 1), arg)
+                                self.eval(program, &mut (last + 1), args)
                             }
                             // function call
                             '(' => {
                                 *index += 1;
                                 let p = self.map.get(&c).unwrap().clone();
-                                let arg = Some(self.eval(program, index, arg)?);
-                                self.eval(p.as_str(), &mut 0, arg)
+                                let mut new_args = vec![];
+                                let last = *index + program[*index..].find(')').unwrap();
+                                while *index < last {
+                                    new_args.push(self.eval(program, index, args.clone())?);
+                                }
+                                self.eval(p.as_str(), &mut 0, new_args)
                             }
                             _ => Err(EvalError::InvalidProgram),
                         }
@@ -52,7 +56,7 @@ impl Parser {
                 }
 
                 // function arg
-                '.' => Ok(arg.unwrap()),
+                'a'..='z' => Ok(args[c as usize - 'a' as usize]),
 
                 // number
                 '0'..='9' => {
@@ -76,12 +80,12 @@ impl Parser {
                             break;
                         }
                     }
-                    self.eval(program, index, arg)
+                    self.eval(program, index, args)
                 }
                 // operators
                 '+' | '-' | '*' | '/' => {
-                    let lhs = self.eval(program, index, arg)?;
-                    let rhs = self.eval(program, index, arg)?;
+                    let lhs = self.eval(program, index, args.clone())?;
+                    let rhs = self.eval(program, index, args)?;
                     match c {
                         '+' => Ok(lhs + rhs),
                         '-' => Ok(lhs - rhs),
@@ -109,7 +113,7 @@ fn main() {
             println!(
                 "{} = {}",
                 program,
-                Parser::new().eval(&program, &mut 0, None).ok().unwrap()
+                Parser::new().eval(&program, &mut 0, vec![]).ok().unwrap()
             );
         }
         _ => {
@@ -120,63 +124,66 @@ fn main() {
 
 #[test]
 fn test_numbers() {
-    assert_eq!(Ok(0), Parser::new().eval("0", &mut 0, None));
-    assert_eq!(Ok(1), Parser::new().eval("1", &mut 0, None));
-    assert_eq!(Ok(123), Parser::new().eval("123", &mut 0, None));
+    assert_eq!(Ok(0), Parser::new().eval("0", &mut 0, vec![]));
+    assert_eq!(Ok(1), Parser::new().eval("1", &mut 0, vec![]));
+    assert_eq!(Ok(123), Parser::new().eval("123", &mut 0, vec![]));
 }
 
 #[test]
 fn test_add() {
-    assert_eq!(Ok(2), Parser::new().eval("+ 1 1", &mut 0, None));
-    assert_eq!(Ok(2), Parser::new().eval("+ 1   1", &mut 0, None));
-    assert_eq!(Ok(3), Parser::new().eval("+ 1 2", &mut 0, None));
-    assert_eq!(Ok(579), Parser::new().eval("+ 123 456", &mut 0, None));
-    assert_eq!(Ok(1000), Parser::new().eval("+ 999 1", &mut 0, None));
+    assert_eq!(Ok(2), Parser::new().eval("+ 1 1", &mut 0, vec![]));
+    assert_eq!(Ok(2), Parser::new().eval("+ 1   1", &mut 0, vec![]));
+    assert_eq!(Ok(3), Parser::new().eval("+ 1 2", &mut 0, vec![]));
+    assert_eq!(Ok(579), Parser::new().eval("+ 123 456", &mut 0, vec![]));
+    assert_eq!(Ok(1000), Parser::new().eval("+ 999 1", &mut 0, vec![]));
 }
 
 #[test]
 fn test_sub() {
-    assert_eq!(Ok(0), Parser::new().eval("- 1 1", &mut 0, None));
-    assert_eq!(Ok(-1), Parser::new().eval("- 1 2", &mut 0, None));
-    assert_eq!(Ok(-333), Parser::new().eval("- 123 456", &mut 0, None));
-    assert_eq!(Ok(998), Parser::new().eval("- 999 1", &mut 0, None));
+    assert_eq!(Ok(0), Parser::new().eval("- 1 1", &mut 0, vec![]));
+    assert_eq!(Ok(-1), Parser::new().eval("- 1 2", &mut 0, vec![]));
+    assert_eq!(Ok(-333), Parser::new().eval("- 123 456", &mut 0, vec![]));
+    assert_eq!(Ok(998), Parser::new().eval("- 999 1", &mut 0, vec![]));
 }
 
 #[test]
 fn test_mul() {
-    assert_eq!(Ok(1), Parser::new().eval("* 1 1", &mut 0, None));
-    assert_eq!(Ok(2), Parser::new().eval("* 1 2", &mut 0, None));
-    assert_eq!(Ok(492), Parser::new().eval("* 123 4", &mut 0, None));
-    assert_eq!(Ok(999), Parser::new().eval("* 999 1", &mut 0, None));
+    assert_eq!(Ok(1), Parser::new().eval("* 1 1", &mut 0, vec![]));
+    assert_eq!(Ok(2), Parser::new().eval("* 1 2", &mut 0, vec![]));
+    assert_eq!(Ok(492), Parser::new().eval("* 123 4", &mut 0, vec![]));
+    assert_eq!(Ok(999), Parser::new().eval("* 999 1", &mut 0, vec![]));
 }
 
 #[test]
 fn test_div() {
-    assert_eq!(Ok(1), Parser::new().eval("/ 1 1", &mut 0, None));
-    assert_eq!(Ok(0), Parser::new().eval("/ 1 2", &mut 0, None));
-    assert_eq!(Ok(2), Parser::new().eval("/ 246 123", &mut 0, None));
+    assert_eq!(Ok(1), Parser::new().eval("/ 1 1", &mut 0, vec![]));
+    assert_eq!(Ok(0), Parser::new().eval("/ 1 2", &mut 0, vec![]));
+    assert_eq!(Ok(2), Parser::new().eval("/ 246 123", &mut 0, vec![]));
     assert_eq!(
         Err(EvalError::ZeroDivision),
-        Parser::new().eval("/ 999 0", &mut 0, None)
+        Parser::new().eval("/ 999 0", &mut 0, vec![])
     );
 }
 
 #[test]
 fn test_multi_operator() {
-    assert_eq!(Ok(10), Parser::new().eval("+ + + 1 2 3 4", &mut 0, None));
-    assert_eq!(Ok(14), Parser::new().eval("+ 2 *4 3", &mut 0, None));
-    assert_eq!(Ok(33), Parser::new().eval("+ 21* 4 3", &mut 0, None));
+    assert_eq!(Ok(10), Parser::new().eval("+ + + 1 2 3 4", &mut 0, vec![]));
+    assert_eq!(Ok(14), Parser::new().eval("+ 2 *4 3", &mut 0, vec![]));
+    assert_eq!(Ok(33), Parser::new().eval("+ 21* 4 3", &mut 0, vec![]));
 }
 
 #[test]
 fn test_function() {
-    assert_eq!(Ok(2), Parser::new().eval("+ . .", &mut 0, Some(1)));
-    assert_eq!(Ok(2), Parser::new().eval("F[+ . .] F(1)", &mut 0, None));
-    assert_eq!(Ok(10), Parser::new().eval("F[* . 2] F(5)", &mut 0, None));
-    assert_eq!(Ok(16), Parser::new().eval("F[* . .] F(F(2))", &mut 0, None));
+    assert_eq!(Ok(2), Parser::new().eval("+ a a", &mut 0, vec![1]));
+    assert_eq!(Ok(2), Parser::new().eval("F[+ a a] F(1)", &mut 0, vec![]));
+    assert_eq!(Ok(10), Parser::new().eval("F[* a 2] F(5)", &mut 0, vec![]));
+    assert_eq!(
+        Ok(16),
+        Parser::new().eval("F[* a a] F(F(2))", &mut 0, vec![])
+    );
     assert_eq!(
         Ok(256),
-        Parser::new().eval("F[* . .] F(F(F(2)))", &mut 0, None)
+        Parser::new().eval("F[* a a] F(F(F(2)))", &mut 0, vec![])
     );
 }
 
@@ -184,19 +191,24 @@ fn test_function() {
 fn test_multi_function() {
     assert_eq!(
         Ok(7),
-        Parser::new().eval("F[* . .] G[+ . 3] G(F(2))", &mut 0, None)
+        Parser::new().eval("F[* a a] G[+ a 3] G(F(2))", &mut 0, vec![])
     );
     assert_eq!(
         Ok(0),
-        Parser::new().eval("F[- . 5] G[F(+ . 5)] G(0)", &mut 0, None)
+        Parser::new().eval("F[- a 5] G[F(+ a 5)] G(0)", &mut 0, vec![])
     );
+}
+
+#[test]
+fn test_multi_arg_function() {
+    assert_eq!(Ok(5), Parser::new().eval("F[+ a b] F(2 3)", &mut 0, vec![]));
 }
 
 #[test]
 fn test_unknown_character() {
     assert_eq!(
         Err(EvalError::UnknownCharacter),
-        Parser::new().eval("?", &mut 0, None)
+        Parser::new().eval("?", &mut 0, vec![])
     );
 }
 
@@ -204,6 +216,6 @@ fn test_unknown_character() {
 fn test_invalid_program() {
     assert_eq!(
         Err(EvalError::InvalidProgram),
-        Parser::new().eval("", &mut 0, None)
+        Parser::new().eval("", &mut 0, vec![])
     );
 }
