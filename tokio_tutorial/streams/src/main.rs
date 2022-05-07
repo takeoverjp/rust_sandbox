474af0e2cf4677884d2c2e4f8b6944a2f56ee06a
Takeo Kondo <takeoverjp@gmail.com>
2022-05-07 19:14:43 +0900

    tokio_tutorial: use stream adapter
    ref: https://tokio.rs/tokio/tutorial/streams#adapters

--- tokio_tutorial/streams/src/main.rs
+++ tokio_tutorial/streams/src/main.rs
@@ -14,7 +14,14 @@ async fn publish() -> mini_redis::Result<()> {
 async fn subscribe() -> mini_redis::Result<()> {
     let client = client::connect("127.0.0.1:6379").await?;
     let subscriber = client.subscribe(vec!["numbers".to_string()]).await?;
-    let messages = subscriber.into_stream();
+    let messages = subscriber
+        .into_stream()
+        .filter(|msg| match msg {
+            Ok(msg) if msg.content.len() == 1 => true,
+            _ => false,
+        })
+        .map(|msg| msg.unwrap().content)
+        .take(3);
 
     tokio::pin!(messages);
 
