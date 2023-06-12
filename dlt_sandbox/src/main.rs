b5f1b7e1ccb25afde3479e9e7cbef5b09313437d
Takeo Kondo <takeoverjp@gmail.com>
2023-06-12 23:30:55 +0900

    dlt_sandbox: suppress warnings

--- dlt_sandbox/src/main.rs
+++ dlt_sandbox/src/main.rs
@@ -3,16 +3,19 @@ use std::ffi::CString;
 #[link(name = "dlt")]
 extern "C" {
 }
-// #![allow(non_upper_case_globals)]
-// #![allow(non_camel_case_types)]
-// #![allow(non_snake_case)]
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+mod dlt{
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(unused)]
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+    
+}
 
 fn main() {
     unsafe {
-        let mut c_str = CString::new("Hello FFI").unwrap();
-        dlt_log_init(DLT_LOG_TO_CONSOLE.try_into().unwrap());
-        dlt_log(DltLogLevelType_DLT_LOG_FATAL, c_str.as_ptr().cast_mut());
+        let c_str = CString::new("Hello FFI").unwrap();
+        dlt::dlt_log_init(dlt::DLT_LOG_TO_CONSOLE.try_into().unwrap());
+        dlt::dlt_log(dlt::DltLogLevelType_DLT_LOG_FATAL, c_str.as_ptr().cast_mut());
     }
 }
