299f12366bf67e8b00d28e7c9c88dd92732d52e0
Takeo Kondo <takeoverjp@gmail.com>
2023-06-13 22:43:43 +0900

    dlt_sandbox: enable white list

--- dlt_sandbox/build.rs
+++ dlt_sandbox/build.rs
@@ -20,6 +20,8 @@ fn main() {
         // bindings for.
         .header("wrapper.h")
         .clang_arg("-I/usr/local/include/dlt/")
+        .allowlist_function("dlt_log.*")
+        .allowlist_var("DLT_LOG_TO_CONSOLE")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
