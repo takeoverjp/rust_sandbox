f37226c78dcbbc36562dd3ebbf7ebd957b11c861
Takeo Kondo <takeoverjp@gmail.com>
2023-06-17 17:19:24 +0900

    exercises: support stop_words

--- exercises/src/main.rs
+++ exercises/src/main.rs
@@ -1,10 +1,24 @@
 use std::io;
 use std::collections::HashMap;
+use std::fs::File;
+use std::path::Path;
+use std::io::Read;
+use std::collections::HashSet;
 
 fn first(line: &str) -> String {
+    let path = Path::new("data/stop_words.txt");
+    let mut file = File::open(&path).unwrap();
+    let mut stop_words = String::new();
+    file.read_to_string(&mut stop_words).unwrap();
+    let stop_words = stop_words.split(',').collect::<HashSet<&str>>();
+    // println!("{:?}", stop_words);
+
     let words = line.split(' ');
     let mut map = HashMap::new();
     for word in words {
+        if stop_words.contains(&word) {
+            continue;
+        }
         let count = map.entry(word).or_insert(0);
         *count += 1;
     }
@@ -32,7 +46,7 @@ mod tests {
     fn test_first() {
         // arrange 
         let input = "White tigers live mostly in India Wild lions live mostly in Africa".to_string();
-        let expected = "live - 2\nmostly - 2\nin - 2\nAfrica - 1\nWhite - 1\nWild - 1\nlions - 1\nAfrica - 1\nIndia - 1\n".to_string();
+        let expected = "live - 2\nmostly - 2\nAfrica - 1\nWhite - 1\nWild - 1\nlions - 1\nAfrica - 1\nIndia - 1\n".to_string();
 
         // act
         let actual = first(&input);
@@ -45,7 +59,7 @@ mod tests {
     fn test_no_same_rank() {
         // arrange 
         let input = "a a a b b c".to_string();
-        let expected = "a - 3\nb - 2\nc - 1\n".to_string();
+        let expected = "b - 2\nc - 1\n".to_string();
 
         // act
         let actual = first(&input);
