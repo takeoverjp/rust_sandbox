0b14acc223f4f8f9fdf2ae24b84777a59d51b43d
Takeo Kondo <takeoverjp@gmail.com>
2023-06-27 23:03:30 +0900

    dlt-tracing-subscriber_sandbox: add other level logs

--- dlt-tracing-subscriber_sandbox/src/main.rs
+++ dlt-tracing-subscriber_sandbox/src/main.rs
@@ -1,4 +1,4 @@
-use tracing::info;
+use tracing::{info, error, warn};
 use tracing_subscriber::prelude::*;
 use dlt_tracing_subscriber::DltLayer;
 use tracing::{info_span, error_span};
@@ -8,11 +8,13 @@ fn main() {
     tracing_subscriber::registry().with(layer).init();
         
     let outer_span = info_span!("outer", level = 0);
-    let _outer_entered = outer_span.enter();
+    let _outer_entered = outer_span.entered();
 
     let inner_span = error_span!("inner", level = 1);
-    let _inner_entered = inner_span.enter();
+    let _inner_entered = inner_span.entered();
 
+    error!(a_bool = true, answer = 42, message = "first example");
     info!(a_bool = true, answer = 42, message = "first example");
+    warn!(a_bool = true, answer = 42, message = "first example");
     println!("Hello, world!");
 }
