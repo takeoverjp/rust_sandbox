02ce3dabd0e9f3c1d89216b491955ed734511b2d
Takeo Kondo <takeoverjp@gmail.com>
2023-06-28 22:56:18 +0900

    dlt-tracing-subscriber_sandbox: add some scoped logs

--- dlt-tracing-subscriber_sandbox/src/main.rs
+++ dlt-tracing-subscriber_sandbox/src/main.rs
@@ -1,20 +1,38 @@
-use tracing::{info, error, warn};
-use tracing_subscriber::prelude::*;
 use dlt_tracing_subscriber::DltLayer;
-use tracing::{info_span, error_span};
+use tracing::{error, info, warn};
+use tracing::{error_span, info_span, trace_span};
+use tracing_subscriber::prelude::*;
 
 fn main() {
-    let layer = DltLayer::new("APP","An example application");
+    let layer = DltLayer::new("APP", "An example application");
     tracing_subscriber::registry().with(layer).init();
-        
-    let outer_span = info_span!("outer", level = 0);
-    let _outer_entered = outer_span.entered();
 
-    let inner_span = error_span!("inner", level = 1);
-    let _inner_entered = inner_span.entered();
+    {
+        let outer_span = info_span!("outer", level = 0);
+        let _outer_entered = outer_span.entered();
 
-    error!(a_bool = true, answer = 42, message = "first example");
-    info!(a_bool = true, answer = 42, message = "first example");
-    warn!(a_bool = true, answer = 42, message = "first example");
+        {
+            let inner_span = error_span!("inner", level = 1);
+            let _inner_entered = inner_span.entered();
+
+            error!(a_bool = true, answer = 42, message = "first example");
+            info!(a_bool = true, answer = 42, message = "first example");
+            warn!(a_bool = true, answer = 42, message = "first example");
+        }
+
+        {
+            let inner_span = trace_span!("inner", level = 1);
+            let _inner_entered = inner_span.entered();
+
+            error!(a_bool = true, answer = 43, message = "second example");
+            info!(a_bool = true, answer = 43, message = "second example");
+            warn!(a_bool = true, answer = 43, message = "second example");
+        }
+    }
+
+    error!(a_bool = true, answer = 44, message = "third example");
+    info!(a_bool = true, answer = 44, message = "third example");
+    warn!(a_bool = true, answer = 44, message = "third example");
+
     println!("Hello, world!");
 }
