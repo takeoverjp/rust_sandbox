8ccc8a4a5f7f4632b56fa8a9f87592e3c58c7b6a
Takeo Kondo <takeoverjp@gmail.com>
2023-07-01 23:13:16 +0900

    dlt-tracing-subscriber_sandbox: add custom layer
    ref: https://burgers.io/custom-logging-in-rust-using-tracing

--- dlt-tracing-subscriber_sandbox/src/main.rs
+++ dlt-tracing-subscriber_sandbox/src/main.rs
@@ -3,6 +3,28 @@ use tracing::{error, info, warn};
 use tracing::{error_span, info_span, trace_span};
 use tracing_subscriber::fmt;
 use tracing_subscriber::prelude::*;
+use tracing_subscriber::Layer;
+
+pub struct CustomLayer;
+
+impl<S> Layer<S> for CustomLayer
+where
+    S: tracing::Subscriber,
+{
+    fn on_event(
+        &self,
+        event: &tracing::Event<'_>,
+        _ctx: tracing_subscriber::layer::Context<'_, S>,
+    ) {
+        println!("Got event!");
+        println!("  level={:?}", event.metadata().level());
+        println!("  target={:?}", event.metadata().target());
+        println!("  name={:?}", event.metadata().name());
+        for field in event.fields() {
+            println!("  field={}", field.name());
+        }
+    }
+}
 
 fn main() {
     let layer = DltLayer::new("APP", "An example application");
@@ -12,6 +34,7 @@ fn main() {
         .with(layer)
         .with(fmt_layer)
         .with(fmt_layer2)
+        .with(CustomLayer)
         .init();
 
     {
